696f9bf7fa7ba8cc39a95cfed46195115cbfa450
cstasney <cstasney07@gmail.com>
2023-10-03 21:05:33 -0400

    api call and buttons working, started for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,51 @@ var apiKey = "054dc6e34b7586a74fbba72f5c2051f9"
 // search button function
 $(document).ready(function () {
     //search button feature
-    $("#search-button").on("click", function () {
-      //get value in input search-value.
-      var searchCity = $("#search-value").val();
-      //empty input field.
-      $("#search-value").val("");
-      getApi(searchCity);
-      getApi(searchCity);
+    $("#search-button").on('click', function () {
+        //get value in input search-value.
+        var city = $("#search-value").val();
+        //clear input field
+        $("#search-value").val("");
+        getWeather(city);
+        getForecast(city);
     });
+})
+
+// makes enter key function as click
+$("#search-value").keyup(function (event) {
+    if (event.keyCode === 13) {
+        $("#search-button").click();
+        getWeather(city);
+        getForecast(city);
+    }
+});
 
 
-function getApi() {
-    var forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=054dc6e34b7586a74fbba72f5c2051f9"
-        fetch(forecast)
-        .then(function(response) {
+// get today's weather
+function getWeather(city) {
+    var weather = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=054dc6e34b7586a74fbba72f5c2051f9&units=metric"
+    fetch(weather)
+        .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)        
+            console.log(data)
         });
 }
 
-getApi();
+function getForecast(city) {
+    var forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=054dc6e34b7586a74fbba72f5c2051f9&units=metric"
+    fetch(forecast)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+    $("#forecast").html("<h2 class=\"mt-3\">5 Day Forecast:</h2>").append("<div class=\row\></div>")
 
-
+    // loop to display card with 5 day forecast
+    // for (var i = 0; i < data.list.length; i++) {
+    //     if(data.list[i].dt_text.indexOf("15:00:00") !== -1)
+    // }
+}
